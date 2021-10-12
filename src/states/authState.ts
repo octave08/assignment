b0fd1c7d9f35ceb8a30b8034b0f69de08ebe6dd9
GYUJEONG HWANG <octave08b@gmail.com>
2021-10-12 20:27:39 +0900

    feat: authState에 persist 적용

--- src/states/authState.ts
+++ src/states/authState.ts
@@ -1,10 +1,14 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 const authState = atom({
   key: "authState",
   default: {
     accessToken: "",
   },
+  effects_UNSTABLE: [persistAtom],
 });
 
 export default authState;
