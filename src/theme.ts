8a4f1235c747161a4311661f82b9e40cbaacf9ff
GYUJEONG HWANG <octave08b@gmail.com>
2021-10-12 16:39:33 +0900

    feat: theme에 colors 업데이트

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,13 @@
 const colors = {
   black: "#000000",
   white: "#FFFFFF",
-  gray90: "#222429"
+
+  gray80: "#3A3E44",
+  gray70: "#1f1f1f",
+  gray60: "#777",
+  gray30: "#EAEBEF",
+
+  pink: "#FF505F",
 };
 
 const theme = {
