89b873506518d03ad6a78038c64f9110c476592e
GYUJEONG HWANG <octave08b@gmail.com>
2021-10-12 16:40:07 +0900

    refactor: 디버깅을 위한 요소 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,4 @@
-import { Switch, Route, useHistory } from "react-router-dom";
-import styled from "styled-components";
-import {
-  compose,
-  flexbox,
-  FlexboxProps,
-  color,
-  ColorProps,
-  typography,
-  TypographyProps,
-} from "styled-system";
+import { Switch, Route } from "react-router-dom";
 
 import {
   LoginPage,
@@ -19,52 +9,9 @@ import {
   PageNotFound,
 } from "./pages";
 
-const Nav = styled.nav<FlexboxProps>`
-  display: flex;
-  ${flexbox}
-`;
-
-const Typography = styled.div<TypographyProps | ColorProps>(
-  compose(typography, color)
-);
-
 const App: React.FC = () => {
-  const history = useHistory();
-
   return (
-    <>
-      <Nav flexDirection="column">
-        <Typography color="gray90">Simple Navigator</Typography>
-        <ul>
-          <li>
-            <button onClick={() => history.push("/")}>
-              로그인 페이지(홈 페이지)
-            </button>
-          </li>
-          <li>
-            <button onClick={() => history.push("/reset-password")}>
-              비밀번호 재설정 페이지
-            </button>
-          </li>
-          <li>
-            <button onClick={() => history.push("/reset-password/verify-code")}>
-              인증 코드 검증 페이지
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={() => history.push("/reset-password/change-password")}
-            >
-              비밀번호 변경 페이지
-            </button>
-          </li>
-          <li>
-            <button onClick={() => history.push("/search-user-info")}>
-              회원 정보 조회 페이지
-            </button>
-          </li>
-        </ul>
-      </Nav>
+    <main>
       <Switch>
         <Route exact path="/">
           <LoginPage />
@@ -83,7 +30,7 @@ const App: React.FC = () => {
         </Route>
         <Route component={PageNotFound} />
       </Switch>
-    </>
+    </main>
   );
 };
 
