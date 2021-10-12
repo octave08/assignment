d8c52c9b4e99501f1b14a75b358923704180682c
GYUJEONG HWANG <octave08b@gmail.com>
2021-10-12 11:14:43 +0900

    feat: 컴포넌트 스타일 기본 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,46 @@
+import styled, { ThemeProvider } from "styled-components";
+import {
+  compose,
+  layout,
+  LayoutProps,
+  flexbox,
+  FlexboxProps,
+  color,
+  ColorProps,
+  typography,
+  TypographyProps,
+} from "styled-system";
+import GlobalStyle from "./GlobalStyle";
+import theme from "./theme";
+
+const Container = styled.div<LayoutProps>`
+  ${layout}
+`;
+
+const Wrapper = styled.div<FlexboxProps>`
+  display: flex;
+  ${flexbox}
+`;
+
+const Typography = styled.div<TypographyProps | ColorProps>(
+  compose(typography, color)
+);
+
 function App() {
   return (
-    <div>
-      <p>Hello Assignment</p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Container width="100%" height="100%">
+        <Wrapper
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <p>Hello Assignment</p>
+          <Typography color="gray90">Hello Styled</Typography>
+        </Wrapper>
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+const colors = {
+  black: "#000000",
+  white: "#FFFFFF",
+  gray90: "#222429"
+};
+
+const theme = {
+  colors,
+};
+
+export default theme;
