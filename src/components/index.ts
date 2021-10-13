986dee4940173e62c9bd7a8addc4c1cdcb6bbf78
GYUJEONG HWANG <octave08b@gmail.com>
2021-10-14 07:49:12 +0900

    chore: 사용하지 않는 component 삭제

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,5 +4,4 @@ export { default as Typography } from "./Typography";
 export { default as Layout } from "./Layout";
 export { default as Margin } from "./Margin";
 export { default as Card } from "./Card";
-export { default as Flex } from "./Flex";
 export { default as Form } from "./Form";
